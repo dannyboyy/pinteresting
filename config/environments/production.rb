c43b3cb8f458db2df0805e89074704ecea17faac
Danny Wu <wushuo1988@hotmail.com>
2014-02-16 22:08:26 -0500

    Implemented Devise for users

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,8 @@ Pinteresting::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  # Required for Heroku/devise
+  # Note to set this to your actual host
+  config.action_mailer.default_url_options = { :host => 'dannywu.herokuapp.com' }    
 end
